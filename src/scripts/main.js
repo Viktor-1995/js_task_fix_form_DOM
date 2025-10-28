0f62f7179ac1675d00696075fe777f9a6c6a1dc1
Viktor-1995 <madebyvityok@ukr.net>
2025-10-28 23:46:29 +0100

    1.0 solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const arrInput = document.querySelectorAll('input');
+
+arrInput.forEach((input) => {
+  const divEl = input.parentElement;
+  const labelEl = document.createElement('label');
+  const inputEl = divEl.querySelector('input');
+  const nameInput = input.getAttribute('name');
+
+  divEl.insertBefore(labelEl, inputEl);
+  labelEl.className = 'field-label';
+  labelEl.setAttribute('for', inputEl.getAttribute('id'));
+  labelEl.textContent = nameInput;
+
+  inputEl.placeholder = nameInput.charAt(0).toUpperCase() + nameInput.slice(1);
+});
